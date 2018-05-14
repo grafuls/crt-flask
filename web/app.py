e94f11063d00d3d411f5946631d9d05241bec124
Gonzalo Rafuls <grafuls@gmail.com>
2018-05-14 19:45:31 +0200

    somewhat functional stuff

--- web/app.py
+++ web/app.py
@@ -1,13 +1,10 @@
-# app.py
-
-
 from flask import Flask
 from flask import request, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from config import BaseConfig
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.config.from_object(BaseConfig)
 db = SQLAlchemy(app)
 
@@ -16,14 +13,27 @@ from models import *
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def professionals():
     if request.method == 'POST':
-        text = request.form['text']
-        post = Post(text)
-        db.session.add(post)
+        name = request.form['name']
+        professional = Professional(name)
+        db.session.add(professional)
         db.session.commit()
-    posts = Post.query.order_by(Post.date_posted.desc()).all()
-    return render_template('index.html', posts=posts)
+    _professionals = Professional.query.order_by(Professional.name.desc()).all()
+    return render_template('professionals.html', professionals=_professionals)
+
+
+@app.route('/patients/<int:professional_id>', methods=['GET', 'POST'])
+def patients(professional_id):
+    if request.method == 'POST':
+        name = request.form['name']
+        insurance = request.form['insurance']
+        sessions = request.form['sessions']
+        patient = Patient(name, insurance, sessions, professional_id)
+        db.session.add(patient)
+        db.session.commit()
+    _patients = Patient.query.filter(Patient.professional_id == professional_id)
+    return render_template('patients.html', patients=_patients, professional_id=professional_id)
 
 
 if __name__ == '__main__':
--- web/models.py
+++ web/models.py
@@ -1,18 +1,62 @@
 # models.py
 
-
-import datetime
+import enum
 from app import db
 
 
-class Post(db.Model):
+class Months(enum.Enum):
+    JAN = 1
+    FEB = 2
+    MAR = 3
+    APR = 4
+    MAY = 5
+    JUN = 6
+    JUL = 7
+    AUG = 8
+    SEP = 9
+    OCT = 10
+    NOV = 11
+    DEC = 12
 
-    __tablename__ = 'posts'
+
+class Professional(db.Model):
+
+    __tablename__ = 'Professionals'
 
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String, nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False)
+    name = db.Column(db.String, nullable=False)
 
-    def __init__(self, text):
-        self.text = text
-        self.date_posted = datetime.datetime.now()
+    def __init__(self, name):
+        self.name = name
+
+
+class Payment(db.Model):
+
+    __tablename__ = 'payments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    month = db.Column(db.Enum(Months), nullable=False)
+    amount = db.Column(db.Numeric)
+    date_payed = db.Column(db.DateTime)
+
+    def __init__(self, month, amount, date_payed):
+        self.month = month
+        self.amount = amount
+        self.date_payed = date_payed
+
+
+class Patient(db.Model):
+
+    __tablename__ = 'Patients'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    insurance = db.Column(db.String)
+    sessions = db.Column(db.Integer)
+    professional_id = db.Column(db.Integer, db.ForeignKey(Professional.id))
+
+    def __init__(self, name, insurance, sessions, professional_id):
+        self.name = name
+        self.insurance = insurance
+        self.sessions = sessions
+        self.professional_id = professional_id
